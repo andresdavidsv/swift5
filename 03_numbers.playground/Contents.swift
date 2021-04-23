ef2916e22b37a92f017b959214d982f6ba19fcee
andresdavidsv <andresdavidsolartevidal@gmail.com>
2021-04-23 14:48:58 -0500

    :memo: Learn about ints

---
+++ 03_numbers.playground/Contents.swift
@@ -0,0 +1,15 @@
+import UIKit
+
+let age : UInt8 = 31
+
+let minValue = UInt8.min // 2^0-1
+let maxValue = UInt8.max // 2^8-1
+
+let minValuInt8 = Int8.min
+let maxValuInt8 = Int8.max
+
+let minValuInt32 = Int32.min
+let maxValuInt32 = Int32.max
+
+let minValuInt64 = Int64.min
+let maxValuInt64 = Int64.max
