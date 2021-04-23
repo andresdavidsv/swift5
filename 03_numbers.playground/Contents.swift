b8bc58cd5ff5f74e529ca52427d713c2649d42af
andresdavidsv <andresdavidsolartevidal@gmail.com>
2021-04-23 15:41:04 -0500

    :memo: Learn casting

--- 03_numbers.playground/Contents.swift
+++ 03_numbers.playground/Contents.swift
@@ -23,3 +23,41 @@ let meaninOfLife = 42 //Int
 let pi = 3.14159 // Double
 
 let anotherPi = 3 + 0.14159
+
+//Literal
+let decimalInteger = 17 //1*10+7
+
+//binary
+let binaryInteger = 0b10001 // Binario
+
+//Octal
+let octalInteger = 0o21 //Octal
+
+//Hexadecimal
+let hexadecimalInteger = 0x11 //Hexa
+
+let decimalDouble = 12.1875
+let exponentDouble = 1.21876e1
+let hexadecimalDouble = 0xC.3p0
+
+//Nomenclatura
+
+let paddedDouble = 0000123.456
+let someNumber = 000000097.54
+
+let oneMillon = 1_000_000
+let justMoreThanAMillion = 1_000_000.000_000_1
+
+//ERORES DE TIPO DE DATO
+//let cannotBeNegative : Unit8 = -1
+//let tooBig : UInt8 = UInt8.max + 1
+
+let twoThousand : UInt16 = 2_0000
+let one : UInt8 = 1
+
+let twoThousandAndOne = twoThousand + UInt16(one)
+let three = 3
+let decimalNumner = 0.14159
+let piNum = Double(three) + decimalNumner
+
+let integerPi = Int(piNum)
