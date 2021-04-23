8dfe99b000fd27312defde746236492f97c5014d
andresdavidsv <andresdavidsolartevidal@gmail.com>
2021-04-23 15:55:59 -0500

    :memo: Learn Booleans

---
+++ 04_data_types.playground/Contents.swift
@@ -0,0 +1,21 @@
+import UIKit
+
+let orangesAreOrange = true
+let foodIsDelicious = false
+
+var isAged : Bool
+
+isAged = true
+
+if isAged {
+    print("Puedes pasar a la fiesta")
+}else{
+    print("No puedes pasar")
+}
+
+var age = 31
+if age >= 18{
+    print("Eres mayor")
+}else{
+    print("no puedes pasar")
+}
