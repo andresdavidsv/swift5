493e5d02bcafed97f9ab2d4fa27eb5e5c4e52853
andresdavidsv <andresdavidsolartevidal@gmail.com>
2021-04-26 15:21:21 -0500

    :memo: Learn about Tuplas

--- 04_data_types.playground/Contents.swift
+++ 04_data_types.playground/Contents.swift
@@ -19,3 +19,17 @@ if age >= 18{
 }else{
     print("no puedes pasar")
 }
+
+ //Tuplas
+
+let http404Error = (404, "Pagina no encontrada")
+let (statusCode,statusMessage) = http404Error
+print("El codigo del estado es: \(statusCode)")
+
+let (justStatusCode,_) = http404Error
+print("El codigo del estado es \(justStatusCode)")
+
+print("El codigo del error es \(http404Error.0) y el mensaje es \(http404Error.1)")
+
+let http200Status = (statusCode:200, description :"Ok")
+print("El codigo del estado es \(http200Status.statusCode) y el mensaje es \(http200Status.description)")
