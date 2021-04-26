cd3842e85ed55fe85609259833ca809f0f4eb1eb
andresdavidsv <andresdavidsolartevidal@gmail.com>
2021-04-26 15:37:07 -0500

    :memo: Learn about nil

--- 04_data_types.playground/Contents.swift
+++ 04_data_types.playground/Contents.swift
@@ -33,3 +33,14 @@ print("El codigo del error es \(http404Error.0) y el mensaje es \(http404Error.1
 
 let http200Status = (statusCode:200, description :"Ok")
 print("El codigo del estado es \(http200Status.statusCode) y el mensaje es \(http200Status.description)")
+
+let posibleAge = "31"
+let convertedAge = Int(posibleAge) //Int?
+
+var serverResponseCode: Int? = 404
+serverResponseCode = nil
+
+var surveyAnswer : String?
+surveyAnswer = "42"
+print(surveyAnswer)
+
